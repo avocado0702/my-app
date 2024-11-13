06fe22083fa88a0384595bd88c01608d93a96557
EPG\JLU <jia.lu@epg.com>
2024-11-13 08:38:23 +0100

    change some css in login page

--- src/main/core/pages/login.cljs
+++ src/main/core/pages/login.cljs
@@ -1,5 +1,6 @@
 (ns main.core.pages.login
-  (:require ["@mui/material/Button" :default Button]
+  (:require ["@mui/material/TextField" :default TextField]
+            ["@mui/material/Button" :default Button]
             ["@mui/material/ButtonGroup" :default ButtonGroup]
             [re-frame.core :as re-frame]
             [reagent.core :as r]))
@@ -12,36 +13,37 @@
     (fn []
       [:div.outer-container
        [:div.login-container [:h2 "Login"]
-        [:form
-         [:div [:label {:for "username"} "Username:"]
-          [:input
-           {:type "text",
-            :id "username",
-            :name "username",
-            :value @username,
-            :on-change #(reset! username (-> %
-                                             .-target
-                                             .-value))}]
-          [:label {:for "password"} "Password:"]
-          [:input
-           {:type "password",
-            :id "password",
-            :name "password",
-            :value @password,
-            :on-change #(reset! password (-> %
-                                             .-target
-                                             .-value))}]
-          [:div.login.error-message [:p @error-message]]
-          [:> ButtonGroup {:variant "text", :sx {:gap "16px"}}
-           [:> Button
-            {:onClick
-               (fn []
-                 (if (and (not (empty? @username)) (not (empty? @password)))
-                   (do (js/console.log "clicked log in")
-                       (re-frame/dispatch [:login-check @username @password]))
-                   (re-frame/dispatch
-                     [:set-login-error-message
-                      "Username and password cannot be empty"])))} "Login"]
-           [:> Button
-            {:onClick #(re-frame/dispatch [:set-current-page :register])}
-            "New Account"]]]]]])))
+        [:div.login-info-container
+         {:style {:display "flex", :flexDirection "column", :gap "16px"}}
+         [:> TextField
+          {:type "text",
+           :required true,
+           :id "username",
+           :label "username",
+           :value @username,
+           :on-change #(reset! username (-> %
+                                            .-target
+                                            .-value))}]
+         [:> TextField
+          {:type "password",
+           :required true,
+           :id "password",
+           :label "password",
+           :value @password,
+           :on-change #(reset! password (-> %
+                                            .-target
+                                            .-value))}]
+         [:div.login.error-message [:p @error-message]]
+         [:> ButtonGroup {:variant "text", :sx {:gap "16px"}}
+          [:> Button
+           {:onClick
+              (fn []
+                (if (and (not (empty? @username)) (not (empty? @password)))
+                  (do (js/console.log "clicked log in")
+                      (re-frame/dispatch [:login-check @username @password]))
+                  (re-frame/dispatch
+                    [:set-login-error-message
+                     "Username and password cannot be empty"])))} "Login"]
+          [:> Button
+           {:onClick #(re-frame/dispatch [:set-current-page :register])}
+           "New Account"]]]]])))
